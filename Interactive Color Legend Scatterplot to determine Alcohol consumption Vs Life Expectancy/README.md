25b0b77cab8ff503a726673a6b5037331e2cbc07
Aishwarya Ramakrishnan <54454914+aishwarya8615@users.noreply.github.com>
2019-12-18 00:13:38 -0500

    Add files via upload

---
+++ Interactive Color Legend Scatterplot to determine Alcohol consumption Vs Life Expectancy/README.md
@@ -0,0 +1,4 @@
+An Interactive Color Legend Scatter plot for [Life Expectancy Data](https://gist.githubusercontent.com/aishwarya8615/89d9f36fc014dea62487f7347864d16a/raw/Life_Expectancy_Data.csv).
+
+This scatterplot takes  required inputs from the data(like Alcohol Consumption, Life Expectancy and Continent)  and uses these as inputs to both X and Y axes with the help of an Interactive Color Legend using 'Continent' as input. This scatter plot would help in determining  the relationship between Alcohol Consumption and Life Expectancy in various continents.
+
---
+++ Interactive Color Legend Scatterplot to determine Alcohol consumption Vs Life Expectancy/index.js
@@ -0,0 +1,127 @@
+import React, { useState, useCallback, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import { csv, scaleLinear, scaleOrdinal, max, format, extent } from 'd3';
+import { useData } from './useData';
+import { AxisBottom } from './AxisBottom';
+import { AxisLeft } from './AxisLeft';
+import { Marks } from './Marks';
+import { ColorLegend } from './ColorLegend';
+
+const width = 960;
+const height = 500;
+const margin = { top: 20, right: 200, bottom: 65, left: 90 };
+const xAxisLabelOffset = 50;
+const yAxisLabelOffset = 45;
+const fadeOpacity = 0.2;
+
+const App = () => {
+  const data = useData();
+  const [hoveredValue, setHoveredValue] = useState(null);
+
+  if (!data) {
+    return <pre>Loading...</pre>;
+  }
+
+  const innerHeight = height - margin.top - margin.bottom;
+  const innerWidth = width - margin.left - margin.right;
+
+  const xValue = d => d.Alcohol;
+  const xAxisLabel = 'Alcohol';
+
+  const yValue = d => d['Life_expectancy '] ;
+  const yAxisLabel = 'Life Expectancy';
+
+  const colorValue = d => d.Continent;
+  const colorLegendLabel = 'Continent';
+
+  const filteredData = data.filter(d => hoveredValue === colorValue(d));
+
+  const circleRadius = 7;
+
+  const siFormat = format('.2s');
+  const xAxisTickFormat = tickValue => siFormat(tickValue).replace('G', 'B');
+
+  const xScale = scaleLinear()
+    .domain(extent(data, xValue))
+    .range([0, innerWidth])
+    .nice();
+
+  const yScale = scaleLinear()
+    .domain(extent(data, yValue))
+    .range([innerHeight, 0]);
+
+  const colorScale = scaleOrdinal()
+    .domain(data.map(colorValue))
+    .range(['maroon', 'purple', 'red', '#ff8c00','#007AFF','#FFF500','#7fa67a','#7ab5e3']);
+
+  return (
+    <svg width={width} height={height}>
+      <g transform={`translate(${margin.left},${margin.top})`}>
+        <AxisBottom
+          xScale={xScale}
+          innerHeight={innerHeight}
+          tickFormat={xAxisTickFormat}
+          tickOffset={5}
+        />
+        <text
+          className="axis-label"
+          textAnchor="middle"
+          transform={`translate(${-yAxisLabelOffset},${innerHeight /
+            2}) rotate(-90)`}
+        >
+          {yAxisLabel}
+        </text>
+        <AxisLeft yScale={yScale} innerWidth={innerWidth} tickOffset={5} />
+        <text
+          className="axis-label"
+          x={innerWidth / 2}
+          y={innerHeight + xAxisLabelOffset}
+          textAnchor="middle"
+        >
+          {xAxisLabel}
+        </text>
+        <g transform={`translate(${innerWidth + 60}, 60)`}>
+          <text x={35} y={-25} className="axis-label" textAnchor="middle">
+            {colorLegendLabel}
+          </text>
+          <ColorLegend
+            tickSpacing={22}
+            tickSize={10}
+            tickTextOffset={12}
+            tickSize={circleRadius}
+            colorScale={colorScale}
+            onHover={setHoveredValue}
+            hoveredValue={hoveredValue}
+            fadeOpacity={fadeOpacity}
+          />
+        </g>
+        <g opacity={hoveredValue ? fadeOpacity : 1}>
+          <Marks
+            data={data}
+            xScale={xScale}
+            xValue={xValue}
+            yScale={yScale}
+            yValue={yValue}
+            colorScale={colorScale}
+            colorValue={colorValue}
+            tooltipFormat={xAxisTickFormat}
+            circleRadius={circleRadius}
+          />
+        </g>
+        <Marks
+          data={filteredData}
+          xScale={xScale}
+          xValue={xValue}
+          yScale={yScale}
+          yValue={yValue}
+          colorScale={colorScale}
+          colorValue={colorValue}
+          tooltipFormat={xAxisTickFormat}
+          circleRadius={circleRadius}
+        />
+      </g>
+    </svg>
+  );
+};
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
